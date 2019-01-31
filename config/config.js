70edadc5bd3beda90b8f51e3b3e4830ff81b56b5
¨Gus <agustincedeno87@gmail.com>
2019-01-31 00:10:11 -0600

    Login de google inicial

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//Google
+module.exports.CLIENT_ID = '926159254646-u43g3lacfssvqqm5hjcpovhuo2ltfv5n.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -3,9 +3,57 @@ var bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 var Usuario = require("../models/usuario");
 var SEED = require("../config/config").SEED;
+
+//Google
+var CLIENT_ID = require("../config/config").CLIENT_ID;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
 // Inicializar variables
 var app = express();
 
+//==================================================
+// Autenticacion por Google
+//==================================================
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+
+    }
+}
+
+app.post('/google', async(req, res) => {
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'Token no Valido'
+            })
+        })
+    return res.status(200).json({
+        ok: true,
+        mensaje: 'OK!!',
+        googleUser: googleUser
+    });
+});
+//==================================================
+// Autenticacion Normal
+//==================================================
 app.post('/', (req, res) => {
     var body = req.body;
     Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
